57a0a2f0ad162b324fb84bfed7790c7cf3238a7d
madebyfabian <beer.fabian@mail.de>
2021-01-31 15:44:44 +0100

    Revert "improved firebase usage"
    This reverts commit 2eef40c108902c57b91cb570c355f29dce728e00.

--- screens/app/ProfileScreen.js
+++ screens/app/ProfileScreen.js
@@ -1,7 +1,7 @@
 import React, { useContext } from 'react'
 import { StyleSheet, Text, Button } from 'react-native'
 
-import { auth } from '../../services/firebase'
+import { logout } from '../../services/firebase'
 import { AuthUserContext } from '../../navigation/AuthUserProvider'
 import SafeView from '../../components/SafeView'
 
@@ -9,10 +9,12 @@ import SafeView from '../../components/SafeView'
 export default function ProfileScreen({ navigation }) {
 	const { user } = useContext(AuthUserContext)
 
+	const handleSignOut = async () => logout()
+
 	return (
 		<SafeView style={ styles.container }>
 			<Text>Hej, {user.email}!</Text>
-			<Button title="Sign Out" onPress={ auth.signOut() } />
+			<Button title="Sign Out" onPress={ handleSignOut } />
 		</SafeView>
 	)
 }
