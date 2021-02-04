45f37f159fa376654d50aedbf7f0ed0cbdf28bba
madebyfabian <beer.fabian@mail.de>
2021-02-04 18:27:43 +0100

    fixed env getting bug

--- services/firebase.js
+++ services/firebase.js
@@ -2,12 +2,15 @@ import * as firebase from 'firebase/app'
 import 'firebase/auth'
 
 
+const generateConfig = () => ({
+  apiKey: process.env.REACT_NATIVE_FIREBASE_APIKEY,
+  authDomain: process.env.REACT_NATIVE_FIREBASE_AUTHDOMAIN,
+})
+
+
 // Initialize Firebase App
 if (!firebase.apps.length)
-  firebase.initializeApp({
-    apiKey: process.env.REACT_NATIVE_FIREBASE_APIKEY,
-    authDomain: process.env.REACT_NATIVE_FIREBASE_AUTHDOMAIN,
-  })
+  firebase.initializeApp(generateConfig())
 
 
 export default firebase
